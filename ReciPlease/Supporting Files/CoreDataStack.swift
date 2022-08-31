ecadbd23d27a2b9c43b1758242b4be55ea30901b
SofyanZa <soza@live.fr>
2022-08-31 15:38:08 +0200

    adding coredatastack

---
+++ ReciPlease/Supporting Files/CoreDataStack.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataStack.swift
+//  ReciPlease
+//
+//  Created by SofyanZ on 31/08/2022.
+//
+
+import Foundation
+import CoreData
+
+open class CoreDataStack {
+    
+    // MARK: - Property
+    
+    private let modelName: String
+    
+    // MARK: - Initializer
+    
+    public init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    // MARK: - Core Data stack
+    
+    public lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: modelName)
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error),\(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    public lazy var mainContext: NSManagedObjectContext = {
+        return persistentContainer.viewContext
+    }()
+    
+    public func saveContext() {
+        guard mainContext.hasChanges else { return }
+        do {
+            try mainContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
